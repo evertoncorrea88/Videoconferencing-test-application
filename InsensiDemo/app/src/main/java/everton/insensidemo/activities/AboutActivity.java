c8697836edefdea62c21794933513e44e6afb3aa
evertoncorrea88 <everton.correa88@gmail.com>
2015-03-14 05:40:56 -0400

    First commit -  app running

---
+++ InsensiDemo/app/src/main/java/everton/insensidemo/activities/AboutActivity.java
@@ -0,0 +1,17 @@
+package everton.insensidemo.activities;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import everton.insensidemo.R;
+
+public class AboutActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
---
+++ InsensiDemo/app/src/main/java/everton/insensidemo/config/OpenTokConfig.java
@@ -0,0 +1,17 @@
+package everton.insensidemo.config;
+
+public class OpenTokConfig {
+
+    // Replace with a generated Session ID
+    public static final String SESSION_ID = "2_MX40NTE3OTIzMn5-MTQyNjI0OTU1NzQ0NH5hSzh1TDhSU1BTMmYzUHNnVFJ0SjA1YnN-fg";
+
+    // Replace with a generated token (from the dashboard or using an OpenTok server SDK)
+    public static final String TOKEN = "T1==cGFydG5lcl9pZD00NTE3OTIzMiZzaWc9MDQ2MTU4ODNlNzJhNzE3M2FkNTFmODE2NGVmNmViMzA5MTc0MjVlYjpyb2xlPXB1Ymxpc2hlciZzZXNzaW9uX2lkPTJfTVg0ME5URTNPVEl6TW41LU1UUXlOakkwT1RVMU56UTBOSDVoU3poMVREaFNVMUJUTW1ZelVITm5WRkowU2pBMVluTi1mZyZjcmVhdGVfdGltZT0xNDI2MjQ5NTY0Jm5vbmNlPTAuMjAxMjM4NTYwMjc4NTg1MjImZXhwaXJlX3RpbWU9MTQyNjg1NDM0OQ==";
+
+    // Replace with your OpenTok API key
+    public static final String API_KEY = "45179232";
+
+    // Subscribe to a stream published by this client. Set to false to subscribe to other clients' streams only.
+    public static final boolean SUBSCRIBE_TO_SELF = false;
+
+}
